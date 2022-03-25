3d6009d0b4f11bafd3989b985b253addaf5179a5
memo099 <lliguerhs@gmail.com>
2022-03-25 13:29:32 -0600

    enumeraciones

--- Exam2/main.cpp
+++ Exam2/main.cpp
@@ -11,6 +11,22 @@
 
 
 using namespace std;
+enum StudentsMenu{
+    printClients=1,
+    RegisterBreakfast,
+    RegisterMeal,
+    RegisterDinner,
+    
+};
+
+enum AdminMenu{
+    EditStudentscapacity=1,
+    Capacityforeachmeal,
+    ModifyHowManyPlatesperBlock,
+    Seethestudentsineachblock,
+    
+};
+
 
 // MISSING: lists
 int ID_GEN() {
@@ -112,6 +128,45 @@ void checkLoginAdmin(vector<Admin> list) { // login as admin
     cout << "Invalid ID or password" << endl;
     checkLoginAdmin(list);
 }
+
+class Admin  {
+    private: int id;
+    
+
+
+    public: void administrador() {
+        system("cls");
+        int id, age;
+        string name, last_name1, last_name2, gender;
+        cout << " ------Administration------ " << endl;
+        cout << "ID: ";
+        cin >> id;
+        cout << " Pleae enter your name: ";
+        cin >> name;
+        cout << " Pleae enter yor fisrt lastname: ";
+        cin >> last_name1;
+        cout << " Pleae enter your second lastname: ";
+        cin >> last_name2;
+        cout << " Enter your age: ";
+        cin >> age;
+        cout << "Enter your gender: ";
+        cin >> gender;
+
+        getAdmin(id, age, name, gender, last_name1, last_name2);
+        system("cls");
+
+    }
+};
+void getClient(int _id, int _age, string _name, string _last_name1, string _lastname2, string _gender){
+    
+    this -> id = _id;
+    this -> age = _age;
+    this -> name = _name;
+    this-> last_name1 = _last_name1;
+    this-> lastname2 = _last_name2;
+    this -> gender = _gender;
+    
+};
 
 int main() {
     showMainMenu();
