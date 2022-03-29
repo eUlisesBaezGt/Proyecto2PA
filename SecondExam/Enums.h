0bc8aba3cfe30376b738cf5ddaa4238cd09043fc
eUlisesBaezGt <0241823@up.edu.mx>
2022-03-29 15:51:32 -0600

    Esto ya corre el main, falta juntar con métodos ya hechos

---
+++ SecondExam/Enums.h
@@ -0,0 +1,34 @@
+#pragma once
+
+enum StudentMenu {
+	LogStud = 1,
+	AddStud,
+	PrAStud,
+	ExitStudent
+};
+
+enum LoggedStudentMenu {
+	DisStudent = 1,
+	EnaStudent,
+	RegBreakfast,
+	RegMeal,
+	RegDinner,
+	ExitStudentLog
+};
+
+enum AdminMenu {
+	LogAdm = 1,
+	AddAdm,
+	PrAAdm,
+	ExitAdmin
+};
+
+enum LoggedAdminMenu {
+	DisAdmin = 1,
+	EnaAdmin,
+	EdStudCap,
+	Cap4Meal,
+	ModBlock,
+	SeeStudentsBlock,
+	ExitAdminLog
+};
---
+++ SecondExam/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+#include "Enums.h"
+
+using namespace std;
+
+int main() {
+	cout << "TRY" << endl;
+	return 0;
+}
