932c9229887a5df55e9ca8b5ccf8e7ad33bf2d90
eUlisesBaezGt <0241823@up.edu.mx>
2022-04-01 13:16:52 -0600

    Update

--- Project/Admin.h
+++ Project/Admin.h
@@ -1,135 +1,135 @@
-#pragma once
-
-#include <iostream>
-#include <cmath>
-#include <ctime>
-#include <cstdio>
-#include <algorithm>
-#include <vector>
-#include <string>
-
-using namespace std;
-
-class Admin
-{
-public:
-	int id{};
-	string password{};
-
-	string name;
-	string middleName;
-	string lastName;
-	int age{};
-	char gender{};
-	bool status = true;
-};
-
-
-bool CheckAdminID(int id, const vector<Admin>& list) {
-    for (auto& i : list) {
-        if (i.id == id) return true; else return false;
-    }
-    return false;
-}
-
-int GetAdminId(int n, vector<Admin>& list) {
-    n = rand() % 100;
-    for (int i = 0; i < list.size(); ++i) {
-        if (!CheckAdminID(n, list)) return n; else GetAdminId(n,list);
-    }
-    return n;
-}
-
-
-
-void EnableAdmin(vector<Admin> list) {
-    bool found = false;
-    cout << "ID of Admin to enable: ";
-    int IS;
-    cin >> IS;
-    cin.ignore();
-    for (auto& i : list) {
-        if (IS == i.id) {
-            found = true;
-            i.status = true;
-            cout << "Admin status successfully updated" << endl;
-        }
-    }
-    if (!found) {
-        cout << "Admin ID not found" << endl;
-    }
-}
-
-void DisableAdmin(vector<Admin> list) {
-    bool found = false;
-    cout << "ID of Admin to disable: ";
-    int IS;
-    cin >> IS;
-    cin.ignore();
-    for (auto& i : list) {
-        if (IS == i.id) {
-            found = true;
-            i.status = false;
-            cout << "Admin status successfully updated" << endl;
-        }
-    }
-    if (!found) {
-        cout << "Admin ID not found" << endl;
-    }
-}
-
-void PrintAdmins(const vector <Admin>& list) {
-    for (auto & i : list) {
-        cout << "-----------------------" << endl;
-        cout << "ID: " << i.id << "\tName: " << i.name << "\tMiddle Name: " << i.middleName << "\tLast Name: " << i.lastName << "\tStatus" << i.status;
-    }
-}
-
-void LoginAdmin(const vector<Admin>& list) {
-    int ID;
-    string Password;
-    cout << "Enter your ID: ";
-    cin >> ID;
-    cin.ignore();
-    cout << "Enter your password: ";
-    getline(cin, Password);
-    /*for (auto& i : list) {
-        if (i.id == ID && i.password == Password && i.status) {
-            cout << "Welcome " << i.name << endl;
-            AdminMenu(list);
-        }
-    }*/
-    cout << "Invalid ID or password" << endl;
-}
-
-void AddAdmin(vector<Admin>& list) {
-    Admin temp;
-    int n=0;
-    temp.id = GetAdminId(n, list);
-    cout << " ------ Add Admin ------ " << endl;
-    cout << "ID: " << temp.id << endl;
-    cout << "Name: ";
-    getline(cin, temp.name);
-    cout << "Middle Name: ";
-    getline(cin, temp.middleName);
-    cout << "Last Name: ";
-    getline(cin, temp.lastName);
-    cout << "Age: ";
-    cin >> temp.age;
-    cin.ignore();
-    cout << "Gender (M-F): ";
-    cin >> temp.gender;
-    cin.ignore();
-    cout << "PASSWORD: ";
-    getline(cin, temp.password);
-    list.push_back(temp);
-
-    system("clear");
-}
-void ChangeCapacity(vector<Admin>& list) {
-    string maxcapacity, changecapacity;
-    int number;
-    cin>> 
-    list.resize()
-}
-
+//#pragma once
+//
+//#include <iostream>
+//#include <cmath>
+//#include <ctime>
+//#include <cstdio>
+//#include <algorithm>
+//#include <vector>
+//#include <string>
+//
+//using namespace std;
+//
+//class Admin
+//{
+//public:
+//	int id{};
+//	string password{};
+//
+//	string name;
+//	string middleName;
+//	string lastName;
+//	int age{};
+//	char gender{};
+//	bool status = true;
+//};
+//
+//
+//bool CheckAdminID(int id, const vector<Admin>& list) {
+//    for (auto& i : list) {
+//        if (i.id == id) return true; else return false;
+//    }
+//    return false;
+//}
+//
+//int GetAdminId(int n, vector<Admin>& list) {
+//    n = rand() % 100;
+//    for (int i = 0; i < list.size(); ++i) {
+//        if (!CheckAdminID(n, list)) return n; else GetAdminId(n,list);
+//    }
+//    return n;
+//}
+//
+//
+//
+//void EnableAdmin(vector<Admin> list) {
+//    bool found = false;
+//    cout << "ID of Admin to enable: ";
+//    int IS;
+//    cin >> IS;
+//    cin.ignore();
+//    for (auto& i : list) {
+//        if (IS == i.id) {
+//            found = true;
+//            i.status = true;
+//            cout << "Admin status successfully updated" << endl;
+//        }
+//    }
+//    if (!found) {
+//        cout << "Admin ID not found" << endl;
+//    }
+//}
+//
+//void DisableAdmin(vector<Admin> list) {
+//    bool found = false;
+//    cout << "ID of Admin to disable: ";
+//    int IS;
+//    cin >> IS;
+//    cin.ignore();
+//    for (auto& i : list) {
+//        if (IS == i.id) {
+//            found = true;
+//            i.status = false;
+//            cout << "Admin status successfully updated" << endl;
+//        }
+//    }
+//    if (!found) {
+//        cout << "Admin ID not found" << endl;
+//    }
+//}
+//
+//void PrintAdmins(const vector <Admin>& list) {
+//    for (auto & i : list) {
+//        cout << "-----------------------" << endl;
+//        cout << "ID: " << i.id << "\tName: " << i.name << "\tMiddle Name: " << i.middleName << "\tLast Name: " << i.lastName << "\tStatus" << i.status;
+//    }
+//}
+//
+//void LoginAdmin(const vector<Admin>& list) {
+//    int ID;
+//    string Password;
+//    cout << "Enter your ID: ";
+//    cin >> ID;
+//    cin.ignore();
+//    cout << "Enter your password: ";
+//    getline(cin, Password);
+//    /*for (auto& i : list) {
+//        if (i.id == ID && i.password == Password && i.status) {
+//            cout << "Welcome " << i.name << endl;
+//            AdminMenu(list);
+//        }
+//    }*/
+//    cout << "Invalid ID or password" << endl;
+//}
+//
+//void AddAdmin(vector<Admin>& list) {
+//    Admin temp;
+//    int n=0;
+//    temp.id = GetAdminId(n, list);
+//    cout << " ------ Add Admin ------ " << endl;
+//    cout << "ID: " << temp.id << endl;
+//    cout << "Name: ";
+//    getline(cin, temp.name);
+//    cout << "Middle Name: ";
+//    getline(cin, temp.middleName);
+//    cout << "Last Name: ";
+//    getline(cin, temp.lastName);
+//    cout << "Age: ";
+//    cin >> temp.age;
+//    cin.ignore();
+//    cout << "Gender (M-F): ";
+//    cin >> temp.gender;
+//    cin.ignore();
+//    cout << "PASSWORD: ";
+//    getline(cin, temp.password);
+//    list.push_back(temp);
+//
+//    system("clear");
+//}
+//void ChangeCapacity(vector<Admin>& list) {
+//    string maxcapacity, changecapacity;
+//    int number;
+//    cin>> 
+//    list.resize()
+//}
+//
--- Project/Menu.h
+++ Project/Menu.h
@@ -14,36 +14,77 @@
 
 using namespace std;
 
-void MainMenu() {
-    vector<Student> students;
-    vector<Admin> admins;
-    char choice;
-    bool run = true;
+void StudentMenu(vector<Student>list)
+{
+	int option;
+	bool run = true;
 
-    while (run) {
-        cout << endl;
-        cout << "A) Use as Admin" << endl;
-        cout << "S) Use as Student" << endl;
-        cout << "Q) Quit" << endl;
-        cout << "Select your option: ";
-        cin >> choice;
-        cin.ignore();
-        switch (choice) {
-        case 'A':
-            cout << "admin "<<endl;
-            // AdminMenu(admins);
-            break;
-        case 'S':
-            cout << "student" << endl;
-            // StudentMenu(students);
-            break;
-        case 'Q':
-            cout << "Goodbye!" << endl;
-            run = false;
-            break;
-        default:
-            cout << "Invalid choice" << endl;
-            break;
-        }
-    }
+	while (run)
+	{
+		cout << "-------- STUDENT MENU -------- " << endl;
+		cout << "1) Login" << endl;
+		cout << "2) Add Student" << endl;
+		cout << "3) Print All" << endl;
+		cout << "4) Exit" << endl;
+
+		cout << "Select your option: ";
+		cin >> option;
+		cin.ignore();
+
+		switch (option)
+		{
+		case LogStud:
+			LoginStud(list);
+			break;
+		case AddStud:
+			AddStudent(list);
+			break;
+		case PrAStud:
+			PrintStudents(list);
+			break;
+		case ExitStudent:
+			cout << "Return to Main Menu" << endl;
+			run = false;
+			break;
+		default:
+			cout << "Invalid option";
+			StudentMenu(list);
+			break;
+		}
+	}
+}
+
+
+void MainMenu() {
+	vector<Student> students;
+	// vector<Admin> admins;
+	char choice;
+	bool run = true;
+
+	while (run) {
+		cout << endl;
+		cout << "A) Use as Admin" << endl;
+		cout << "S) Use as Student" << endl;
+		cout << "Q) Quit" << endl;
+		cout << "Select your option: ";
+		cin >> choice;
+		cin.ignore();
+		switch (choice) {
+		case 'A':
+			cout << "admin " << endl;
+			// AdminMenu(admins);
+			break;
+		case 'S':
+			cout << "student" << endl;
+			StudentMenu(students);
+			break;
+		case 'Q':
+			cout << "Goodbye!" << endl;
+			run = false;
+			break;
+		default:
+			cout << "Invalid choice" << endl;
+			break;
+		}
+	}
 }
