d213363f3f92f6093259d81741a07aa90d9eea24
eUlisesBaezGt <0241823@up.edu.mx>
2022-04-01 20:32:55 -0600

    Aquí vamos de nuevo a probar cosa por cosa

--- Project/Student.h
+++ Project/Student.h
@@ -5,11 +5,9 @@
 #include <vector>
 #include <string>
 
-#include "Enums.h"
-#include "Menu.h"
-
 class Student;
 using namespace std;
+
 
 struct Breakfast
 {
@@ -55,22 +53,32 @@ public:
 
 };
 
-bool CheckStudentID(int id, const vector<Student> list) {
-	bool b = false;
+
+inline bool CheckStudentID(int id, const vector<Student> list) {
 	for (auto& i : list) {
 		if (i.id == id) return true; break;
 	}
-	return b;
+	return false;
 }
 
-int GetStudentId(int n, const vector<Student> list) {
+inline int GetStudentId(int n, const vector<Student> list) {
 	n = rand() % 100;
 	for (int i = 0; i < list.size(); ++i) {
 		if (!CheckStudentID(n, list)) return n; break;
 	}
+	return 0;
 }
 
-void AddStudent(vector<Student>& list) {
+inline void PrintStudents(const vector<Student> list) {
+	for (auto& i : list) {
+		cout << "-----------------------" << endl;
+		cout << "ID: " << i.id << "\tName: " << i.name << "\tMiddle Name: "
+			<< i.middleName << "\tLast Name: " << i.lastName << "\nStatus"
+			<< i.status;
+	}
+}
+
+inline void AddStudent(vector<Student>& list) {
 	Student temp;
 	int n = 0;
 	temp.id = GetStudentId(n, list);
@@ -95,92 +103,3 @@ void AddStudent(vector<Student>& list) {
 	system("clear");
 }
 
-void PrintStudents(const vector<Student> list) {
-	for (auto& i : list) {
-		cout << "-----------------------" << endl;
-		cout << "ID: " << i.id << "\tName: " << i.name << "\tMiddle Name: " << i.middleName
-			<< "\tLast Name: " << i.lastName << "\nStatus" << i.status;
-	}
-}
-
-void EnableStudent(vector<Student>& list) {
-	bool found = false;
-	cout << "ID of Student to enable: ";
-	int IS;
-	cin >> IS;
-	cin.ignore();
-	for (auto& i : list) {
-		if (IS == i.id) {
-			found = true;
-			i.status = true;
-			cout << "Student status successfully updated" << endl;
-		}
-	}
-	if (!found) {
-		cout << "Student ID not found" << endl;
-	}
-}
-
-void DisableStudent(vector<Student>& list) {
-	bool found = false;
-	cout << "ID of Student to disable: ";
-	int IS;
-	cin >> IS;
-	cin.ignore();
-	for (auto& i : list) {
-		if (IS == i.id) {
-			found = true;
-			i.status = false;
-			cout << "Student status successfully updated" << endl;
-		}
-	}
-	if (!found) {
-		cout << "Student ID not found" << endl;
-	}
-}
-
-void Log_Stud(vector<Student>& students) {
-	bool run = true;
-	int option;
-
-	while (run)
-	{
-		cout << "-------- LOGGED STUDENT MENU -------- ";
-		cout << "1) Disable Student" << endl;
-		cout << "2) Enable Student" << endl;
-		cout << "3) Register Breakfast" << endl;
-		cout << "4) Register Meal" << endl;
-		cout << "5) Register Dinner" << endl;
-		cout << "6) Exit" << endl;
-		cout << "Select your option: ";
-
-		cin >> option;
-		cin.ignore();
-		switch (option) {
-		case DisStudent:
-			cout << "Disable Student" << endl;
-			DisableStudent(students);
-			break;
-		case EnaStudent:
-			cout << "Enable Student" << endl;
-			EnableStudent(students);
-			break;
-		case RegBreakfast:
-			cout << "Register Breakfast" << endl;
-			break;
-		case RegMeal:
-			cout << "Register Meal" << endl;
-			break;
-		case RegDinner:
-			cout << "Register Dinner" << endl;
-			break;
-		case ExitStudentLog:
-			cout << "Return to Main Menu" << endl;
-			run = false;
-			break;
-		default:
-			cout << "Invalid option";
-			break;
-		}
-	}
-}
